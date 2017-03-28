693fe696c66c97c16e365ebad93ae1249bc4314c
Sylvain <alonso.sylvain@outlook.com>
2017-03-28 09:54:53 +0200

    Adding right tables

--- app/src/main/java/be/ecam/ecalendar/CalendarContract.java
+++ app/src/main/java/be/ecam/ecalendar/CalendarContract.java
@@ -6,16 +6,24 @@ import android.provider.BaseColumns;
  * Created by Sylvain on 21-03-17.
  */
 
-public class CalendarContract {
-    public static final class CalendarEntry implements BaseColumns {
-        public static final String TABLE_NAME = "calendar";
-        public static final String CALENDAR_ACTIVITY_ID = "activity_id";
-        public static final String CALENDAR_ACTIVITY_NAME = "activity_name";
-        public static final String CALENDAR_START_TIME = "start_time";
-        public static final String CALENDAR_END_TIME = "end_time";
-        public static final String CALENDAR_GROUP = "group";
-        public static final String CALENDAR_TEACHER = "teacher";
-        public static final String CALENDAR_CLASS_ROOM = "class_room";
-        public static final String CALENDAR_NOTE = "note";
+ public class CalendarContract {
+    public static final class ScheduleEntry implements BaseColumns {
+        public static final String TABLE_NAME = "schedule";
+        public static final String SCHEDULE_ACTIVITY_ID = "activity_id";
+        public static final String SCHEDULE_ACTIVITY_NAME = "activity_name";
+        public static final String SCHEDULE_START_TIME = "start_time";
+        public static final String SCHEDULE_END_TIME = "end_time";
+        public static final String SCHEDULE_GROUP = "group";
+        public static final String SCHEDULE_TEACHER = "teacher";
+        public static final String SCHEDULE_CLASS_ROOM = "class_room";
     }
+
+    public static final class CalendarTypeEntry implements BaseColumns{
+        public static final String TABLE_NAME = "calendar_type";
+        public static final String CALENDAR_TYPE_ID = "id";
+        public static final String CALENDAR_TYPE_DESCRIPTION = "description";
+    }
+
 }
+
+
