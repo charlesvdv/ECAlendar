79aa1006921fdb8009fe536b7ab2d83ac811737e
Charles Vandevoorde <charles.vandevoorde@hotmail.be>
2017-04-05 14:18:30 +0200

    handle error in loader

--- app/src/main/java/be/ecam/ecalendar/CalendarLoader.java
+++ app/src/main/java/be/ecam/ecalendar/CalendarLoader.java
@@ -51,46 +51,61 @@ public class CalendarLoader extends IntentService {
         super(name);
     }
 
-    @Override
-    protected void onHandleIntent(Intent intent) {
-        Intent returnIntent = new Intent(BROADCAST_ACTION);
-        switch(intent.getStringExtra("action")) {
-            case "schedule":
-                String name = intent.getStringExtra("name");
-                ArrayList<Schedule> schedules = getSchedules(name);
-
-                returnIntent.putExtra("action", "schedule");
-                returnIntent.putExtra("schedules", schedules);
-                returnIntent.putExtra("name", name);
-                break;
-
-            case "type":
-                ArrayList<CalendarType> teachers = getCalendarType(TEACHERS_URL, "trigens", "npens");
-                ArrayList<CalendarType> classrooms = getCalendarType(CLASSROOMS_URL, "abraud", "nomaud");
-                ArrayList<CalendarType> groups = getCalendarType(GROUPS_URL, "abrser", "demisérie");
-                // Could be handled when we have a proper API...
-                // ArrayList<CalendarType> students = getCalendarType(STUDENTS_URL);
-
-                returnIntent.putExtra("action", "type");
-                returnIntent.putExtra("teachers", teachers);
-                returnIntent.putExtra("classrooms", classrooms);
-                returnIntent.putExtra("groups", groups);
-                break;
-
-            default:
-                Log.d(TAG, "Could not match this action.");
-        }
-        LocalBroadcastManager.getInstance(this).sendBroadcast(returnIntent);
+    public CalendarLoader() {
+        this(CalendarLoader.class.getSimpleName());
     }
 
-    private ArrayList<CalendarType> getCalendarType(String url, String idKey, String descriptionKey) {
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        requestQueue = Volley.newRequestQueue(this);
+
+        Intent returnIntent = new Intent(this, CalendarLoaderReceiver.class);
+        try {
+            switch(intent.getStringExtra("action")) {
+                case "schedule":
+                    String name = intent.getStringExtra("name");
+                    ArrayList<Schedule> schedules = getSchedules(name);
+
+                    returnIntent.putExtra("action", "schedule");
+                    returnIntent.putExtra("name", name);
+
+                    returnIntent.putParcelableArrayListExtra("schedules", schedules);
+                    break;
+
+                case "type":
+                    ArrayList<CalendarType> teachers = getCalendarType(TEACHERS_URL, "trigens", "npens");
+                    ArrayList<CalendarType> classrooms = getCalendarType(CLASSROOMS_URL, "abraud", "nomaud");
+                    ArrayList<CalendarType> groups = getCalendarType(GROUPS_URL, "abrser", "demisérie");
+                    // Could be handled when we have a proper API...
+                    // ArrayList<CalendarType> students = getCalendarType(STUDENTS_URL);
+
+                    returnIntent.putExtra("action", "type");
+                    returnIntent.putParcelableArrayListExtra("teachers", teachers);
+                    returnIntent.putParcelableArrayListExtra("classrooms", classrooms);
+                    returnIntent.putParcelableArrayListExtra("groups", groups);
+                    break;
+
+                default:
+                    Log.d(TAG, "Could not match this action.");
+            }
+        } catch (NetworkException | ICSParsingException e) {
+            returnIntent.putExtra("action", "error");
+            returnIntent.putExtra("message", e.getMessage());
+            Log.d(TAG, "Error send");
+        }
+        //LocalBroadcastManager.getInstance(this).sendBroadcast(returnIntent);
+        sendBroadcast(returnIntent);
+    }
+
+    private ArrayList<CalendarType> getCalendarType(String url, String idKey, String descriptionKey) throws NetworkException {
         // Synchronous request.
         RequestFuture<JSONArray> future = RequestFuture.newFuture();
         JsonArrayRequest request = new JsonArrayRequest(url, future, future);
+        requestQueue.add(request);
 
         ArrayList<CalendarType> types = new ArrayList<>();
         try {
-            JSONArray response = future.get();
+            JSONArray response = future.get(TIMEOUT, TimeUnit.SECONDS);
             for (int i = 0; i < response.length(); i++) {
                 JSONObject data = (JSONObject) response.get(i);
                 types.add(new CalendarType(
@@ -98,24 +113,25 @@ public class CalendarLoader extends IntentService {
                         data.getString(descriptionKey)
                 ));
             }
-        } catch (InterruptedException | ExecutionException | JSONException e) {
+        } catch (Exception e) {
             e.printStackTrace();
+            throw new NetworkException(e.getCause());
         }
 
         return types;
     }
 
-    private ArrayList<Schedule> getSchedules(String name) {
+    private ArrayList<Schedule> getSchedules(String name) throws ICSParsingException, NetworkException {
         // Synchronous request.
         RequestFuture<String> future = RequestFuture.newFuture();
         StringRequest request = new StringRequest(ICS_URL + name, future, future);
+        requestQueue.add(request);
 
         ArrayList<Schedule> schedules = new ArrayList<>();
         try {
             // Parse the ICS data.
-            StringReader reader = new StringReader(future.get());
+            StringReader reader = new StringReader(future.get(TIMEOUT, TimeUnit.SECONDS));
             Calendar calendar = new CalendarBuilder().build(reader);
-
             // Iterate over event data
             for (Object o : calendar.getComponents()) {
                 Component component = (Component) o;
@@ -126,12 +142,17 @@ public class CalendarLoader extends IntentService {
                 }
 
                 VEvent event = (VEvent) component;
-                schedules.add(convertVEventToSchedule((event)));
+                try {
+                    schedules.add(convertVEventToSchedule(name, event));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    throw new ICSParsingException(e.getCause());
+                }
             }
-        } catch (InterruptedException | ExecutionException | ParserException | IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
+            throw new NetworkException(e.getCause());
         }
-
         return schedules;
     }
 
@@ -143,11 +164,16 @@ public class CalendarLoader extends IntentService {
         Date endDate = new Date();
         event.getConsumedTime(startDate, endDate);
 
-        ParameterList parameters = event.getDescription().getParameters();
-        String teacher = parameters.getParameter("Ens").getValue();
-        String name = parameters.getParameter("Act").getValue();
-        String group = parameters.getParameter("Groupe").getValue();
+    // Define custom exceptions.
+    class NetworkException extends Exception {
+        public NetworkException(Throwable throwable) {
+            super("Could not load data", throwable);
+        }
+    }
 
-        return new Schedule(id, startDate, endDate, name, group, teacher, classRoom);
+    class ICSParsingException extends Exception {
+        public ICSParsingException(Throwable throwable) {
+            super("Error while parsing ICS", throwable);
+        }
     }
 }
