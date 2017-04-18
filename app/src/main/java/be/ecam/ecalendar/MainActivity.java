85be261ddf72b8b18b86cffd549af574d0319b40
Charles Vandevoorde <charles.vandevoorde@hotmail.be>
2017-04-18 11:13:58 +0200

    offset the calendar to the current date

--- app/src/main/java/be/ecam/ecalendar/MainActivity.java
+++ app/src/main/java/be/ecam/ecalendar/MainActivity.java
@@ -6,10 +6,18 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity implements CalendarAdapter.CalendarAdapterOnClickHandler {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity
+        implements CalendarDAO.CalendarDataUpdated, CalendarAdapter.CalendarAdapterOnClickHandler {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    RecyclerView.LayoutManager layoutManager;
+    CalendarAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +27,13 @@ public class MainActivity extends AppCompatActivity implements CalendarAdapter.C
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.resultView);
         recyclerView.setHasFixedSize(true);
 
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
 
-        CalendarAdapter adapter = new CalendarAdapter(this);
-        CalendarDAO dao = CalendarDAO.createSingleton(this, adapter);
-        adapter.setCalendarData("serie_4EI5A", dao.getCalendar("serie_4EI5A"));
-        adapter.setCalendarData("serie_4EM2A", dao.getCalendar("serie_4EM2A"));
+        adapter = new CalendarAdapter(this);
+        CalendarDAO dao = CalendarDAO.createSingleton(this, this);
+        dao.getCalendar("serie_4EI5A");
+        dao.getCalendar("serie_4EM2A");
         dao.getCalendarTypes();
         recyclerView.setAdapter(adapter);
     }
@@ -50,14 +58,20 @@ public class MainActivity extends AppCompatActivity implements CalendarAdapter.C
         }
 
         //Place here others menu items with an if
-
         return super.onOptionsItemSelected(item);
     }
 
     @Override
     public void onClick(Schedule schedule) {
-        Intent intent = new Intent (this,DetailActivity.class);
+        Intent intent = new Intent(this, DetailActivity.class);
         intent.putExtra("schedule", schedule);
         startActivity(intent);
+    }
+
+    @Override
+    public void notifySchedulesChange(String name, ArrayList<Schedule> schedules) {
+        adapter.setCalendarData(name, schedules);
+        layoutManager.scrollToPosition(adapter.getCalendarPosition());
+
     }
 }
