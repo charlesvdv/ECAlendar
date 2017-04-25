caa1588f8db92a7bc04bfbd4f90410960a9777a4
antoinevdm <antoine_vdm@hotmail.fr>
2017-04-25 10:45:35 +0200

    Asynchornous bug fixing for dynamic listPreference fill

--- app/src/main/java/be/ecam/ecalendar/SettingsFragment.java
+++ app/src/main/java/be/ecam/ecalendar/SettingsFragment.java
@@ -3,8 +3,12 @@ package be.ecam.ecalendar;
 import android.os.Bundle;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.util.Log;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 /**
@@ -18,17 +22,30 @@ public class SettingsFragment extends PreferenceFragmentCompat
     public void onCreatePreferences(Bundle bundle, String s) {
         addPreferencesFromResource(R.xml.pref_settings);
 
-        CalendarDAO DAO = new CalendarDAO()
+        CalendarDAO dao = CalendarDAO.createSingleton(getActivity(), this);
+        dao.getCalendarTypes();
     }
 
     @Override
     public void notifySchedulesChange(String name, ArrayList<Schedule> schedules) {
+        //Nothing to do
+    }
+
+    @Override
+    public void notifyCalendarTypesChanges(HashMap<String, ArrayList<CalendarType>> types) {
+        Log.d("grgtgtrg", "czlledd!!");
+        ArrayList<CalendarType> entries = new ArrayList<>();
+        for (Map.Entry<String, ArrayList<CalendarType>> typeList : types.entrySet()) {
+            entries.addAll(typeList.getValue());
+        }
+
+        CharSequence[] calendarType = entries.toArray(new CharSequence[entries.size()]);
+
         ListPreference listPreference = (ListPreference) findPreference("pref_section");
 
-        CharSequence[] entries = { "English", "French"};
-        CharSequence[] entryValues = {"1" , "2"};
+        CharSequence[] test = { "English", "French"};
 
-        listPreference.setEntries(entries);
-        listPreference.setEntryValues(entryValues);
+        listPreference.setEntries(test);
+        listPreference.setEntryValues(test);
     }
 }
